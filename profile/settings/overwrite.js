26d32af736e2e37128ef3201f43e9fb86b6a7522
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-04-10 14:05:41 -0500

    Added tag overwrite

--- profile/settings/overwrite.js
+++ profile/settings/overwrite.js
@@ -2,13 +2,17 @@ const emailInput = document.querySelector('input[name="signup-email"]');
 const passwordInput = document.querySelector('input[name="signup-password"]');
 const nameInput = document.querySelector('input[name="name"]');
 const saveButton = document.querySelector('#save');
-    
+const selectTag = document.getElementById('tag');
+
 saveButton.addEventListener('click', function() {
   localStorage.setItem('signup-email', emailInput.value);
   localStorage.setItem('signup-password', passwordInput.value);
   localStorage.setItem('user-name', nameInput.value);
+  
+  const selectedTag = selectTag.value;
+  localStorage.setItem('tag', selectedTag);
 });
-    
+
 const storedEmail = localStorage.getItem('signup-email');
 const storedPassword = localStorage.getItem('signup-password');
 const storedName = localStorage.getItem('user-name');
@@ -22,4 +26,13 @@ if (storedName) {
   userNameElem.insertAdjacentElement('afterend', profileLink);
 } else {
   userNameElem.textContent = 'Unknown';
+}
+
+if (typeof(Storage) !== "undefined") {
+  const tagValue = localStorage.getItem('tag');
+  if (tagValue) {
+    selectTag.value = tagValue;
+  }
+} else {
+  console.log('Sorry! No Web Storage support..');
 }
