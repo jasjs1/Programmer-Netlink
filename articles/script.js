3831dcfa98292f79882dbe438522459addfdf492
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-04-15 21:06:55 -0500

    removed comments

--- articles/script.js
+++ articles/script.js
@@ -2,24 +2,18 @@ const savedName = localStorage.getItem('signup-name');
 const form = document.querySelector('form');
 const articlesContainer = document.getElementById('articles');
 
-
-// Update the page title to include the user's name
-// document.title = `${name}'s Article Creator`;
-
-// Load existing articles from local storage
 const savedArticles = JSON.parse(localStorage.getItem('articles')) || [];
 
-// Render existing articles
+
 if (savedArticles.length) {
   const articlesHTML = savedArticles.map(article => `<div class="article"><h3>${article.name}</h3><h2>${article.title}</h2><p>${article.body}</p></div>`).join('');
   articlesContainer.innerHTML = articlesHTML;
 }
 
-// Add new article and save to local storage
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  // Get the input values
+
   const title = document.getElementById('article-title').value.trim();
   const body = document.getElementById('article-body').value.trim();
 
@@ -29,17 +23,17 @@ form.addEventListener('submit', (event) => {
     body,
   };
 
-  // Save the new article to local storage
+
   savedArticles.push(article);
   localStorage.setItem('articles', JSON.stringify(savedArticles));
 
-  // Add the new article to the list
+
   const articleElement = document.createElement('div');
   articleElement.classList.add('article');
   articleElement.innerHTML = `<h2>${title}</h2><p>${body}</p>`;
   articlesContainer.appendChild(articleElement);
 
-  // Reset the form
+
   form.reset();
 });
 
@@ -69,13 +63,9 @@ showContainerButton.addEventListener('click', function() {
 });
 
 
-// Check if articles data exists in local storage
 if (localStorage.getItem('articles')) {
-  // Render the articles data
   const articles = JSON.parse(localStorage.getItem('articles'));
-  // Code to display the articles on the page
 } else {
-  // No articles data found, display a friendly message to the user
   const message = document.createElement('p');
   message.textContent = savedName + ' has not created any articles, yet.';
   document.body.appendChild(message);
