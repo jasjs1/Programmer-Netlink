2f54b27678091cf368f8a579b0395694887dd90c
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-04-17 20:11:05 -0500

    updated some things

--- profile/script.js
+++ profile/script.js
@@ -1,6 +1,124 @@
 window.onload = () => {
     const followButton = document.getElementById("follow-button");
+    const unfollowButton = document.getElementById("unfollow-button");awindow.onload = () => {
+    const followButton = document.getElementById("follow-button");
     const unfollowButton = document.getElementById("unfollow-button");
+    const followerCountElement = document.getElementById("follower-count");
+
+    // Check if there's a stored follower count in local storage
+    let followerCount = localStorage.getItem("followerCount");
+    if (!followerCount) {
+      followerCount = 0;
+    }
+
+    // Update the follower count element with the current follower count
+    followerCountElement.textContent = followerCount;
+
+    // Update the button text based on whether the user is following or not
+    if (followerCount > 0) {
+      followButton.style.display = "none";
+      unfollowButton.style.display = "inline-block";
+    } else {
+      followButton.style.display = "inline-block";
+      unfollowButton.style.display = "none";
+    }
+
+    // Add a click event listener to the follow button
+    followButton.addEventListener("click", () => {
+      // Add 1 to the follower count
+      followerCount++;
+
+      // Save the updated follower count to local storage
+      localStorage.setItem("followerCount", followerCount);
+
+      // Update the follower count element and button display based on whether the user is following or not
+      followerCountElement.textContent = followerCount;
+      followButton.style.display = "none";
+      unfollowButton.style.display = "inline-block";
+    });
+
+    // Add a click event listener to the unfollow button
+    unfollowButton.addEventListener("click", () => {
+      // Subtract 1 from the follower count
+      followerCount--;
+
+      // Save the updated follower count to local storage
+      localStorage.setItem("followerCount", followerCount);
+
+      // Update the follower count element and button display based on whether the user is following or not
+      followerCountElement.textContent = followerCount;
+      followButton.style.display = "inline-block";
+      unfollowButton.style.display = "none";
+    });
+  };
+
+  
+  // Check if there is a saved image in localStorage and display it
+  window.addEventListener('load', () => {
+    const profileImage = localStorage.getItem('profileImage');
+    if (profileImage) {
+      const uploadedImage = document.getElementById('uploaded-image');
+      uploadedImage.style.backgroundImage = `url(${profileImage})`;
+      uploadedImage.style.display = 'block';
+    }
+  });
+  
+  
+  
+  const containerDiv = document.querySelector('.container');
+  const showContainerButton = document.getElementById('show-container');
+
+function checkTitleLength() {
+  const titleValue = titleInput.value;
+  if (titleValue.length < titleInput.minLength) {
+    console.log(`Title must be at least ${titleInput.minLength} characters`);
+    toggleContainer('show');
+  } else if (titleValue.length > titleInput.maxLength) {
+    console.log(`Title must be no more than ${titleInput.maxLength} characters`);
+    toggleContainer('show');
+  } else {
+    console.log(""); // empty console.log statement to clear any previous messages
+    toggleContainer('hide');
+  }
+
+  // Function to check the length of the body inputs
+  const bodyValue = bodyInput.value;
+  if (bodyValue.length < bodyInput.minLength) {
+    console.log(`Body must be at least ${bodyInput.minLength} characters`);
+    toggleContainer('show');
+  } else if (bodyValue.length > bodyInput.maxLength) {
+    console.log(`Body must be no more than ${bodyInput.maxLength} characters`);
+    toggleContainer('show');
+  } else {
+    console.log(""); // empty console.log statement to clear any previous messages
+    toggleContainer('hide');
+  }
+}
+
+function toggleContainer(action) {
+  if (action === 'show') {
+    containerDiv.style.display = 'block';
+    showContainerButton.textContent = '-';
+  } else if (action === 'hide') {
+    containerDiv.style.display = 'none';
+    showContainerButton.textContent = '+';
+  } else if (action === 'toggle') {
+    if (containerDiv.style.display === 'none') {
+      containerDiv.style.display = 'block';
+      showContainerButton.textContent = '-';
+    } else {
+      containerDiv.style.display = 'none';
+      showContainerButton.textContent = '+';
+    }
+  }
+}
+
+showContainerButton.addEventListener('click', function() {
+  toggleContainer('toggle');
+});
+
+
+
     const followerCountElement = document.getElementById("follower-count");
 
     // Check if there's a stored follower count in local storage
