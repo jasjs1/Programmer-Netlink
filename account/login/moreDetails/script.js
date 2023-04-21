54d3f2c287e457c2eadacda62c4108142f288b65
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-04-20 19:29:51 -0500

    ic

---
+++ account/login/moreDetails/script.js
@@ -0,0 +1,55 @@
+body {
+    background-color: #1c1f23;
+    color: white;
+    font-family: 'Open Sans', sans-serif;
+}
+
+
+.head {
+    color: #6466E9;
+    font-weight: lighter;
+}
+
+input[type="text"] {
+    background-color: #1c1f23;
+    border: 1px solid;
+    border-color: #6466E9;
+    width: 825px;
+    height: 45px;
+    border: 1px solid;
+    border-color: #6466E9;
+    border-radius: 5px;
+    padding: 15px;
+    color: white;
+}
+
+select {
+    /* center selector */
+    background-color: #1c1f23;
+    border: 1px solid;
+    border-color: #6466E9;
+    width: 825px;
+    height: 45px;
+    color: white;
+    font-weight: normal;
+    font-size: 18px;
+    border-radius: 5px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: #6466E9;
+    border-image: initial;
+    border-image-slice: initial;
+    border: 1px solid;
+    border-color: #6466E9;
+    border-radius: 5px;
+    padding: 15px;
+}
+
+button {
+    background-color: #1c1f23;
+    color: white;
+    border: 1px solid;
+    border-color: #6466E9;
+    border-radius: 5px;
+    padding: 15px;
+}
