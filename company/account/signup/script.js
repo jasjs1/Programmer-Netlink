cf2f95d77f8a6e6f979de579d4e3de60f05d98b3
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-04-29 15:00:22 -0500

    removed comments, alert and added redirect when info is saved

--- company/account/signup/script.js
+++ company/account/signup/script.js
@@ -1,19 +1,16 @@
-// Get the form and the input fields
 const form = document.querySelector('form');
 const companyNameInput = document.getElementById('comp-name');
 const companyEmailInput = document.getElementById('comp-email');
 const companyPasswordInput = document.getElementById('comp-password');
 
-// Add an event listener to the form for when it's submitted
+
 form.addEventListener('submit', function(event) {
-  // Prevent the form from submitting normally
+
   event.preventDefault();
 
-  // Save the input values to local storage
   localStorage.setItem('comp-name', companyNameInput.value);
   localStorage.setItem('comp-email', companyEmailInput.value);
   localStorage.setItem('comp-password', companyPasswordInput.value);
 
-  // Optionally, display a message to confirm that the data was saved
-  alert('Information saved!');
+  window.location.href = 'http://127.0.0.1:3000/company/account/.moredetials/comp-more-details.html';
 });
