04d337c1600595ce6c0919ce08442cf61d07f711
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-04-29 19:12:58 -0500

    added logo save to localstorage

--- company/account/signup/script.js
+++ company/account/signup/script.js
@@ -2,15 +2,25 @@ const form = document.querySelector('form');
 const companyNameInput = document.getElementById('comp-name');
 const companyEmailInput = document.getElementById('comp-email');
 const companyPasswordInput = document.getElementById('comp-password');
-
+const companyLogoInput = document.getElementById('comp-logo');
 
 form.addEventListener('submit', function(event) {
 
   event.preventDefault();
 
-  localStorage.setItem('comp-name', companyNameInput.value);
-  localStorage.setItem('comp-email', companyEmailInput.value);
-  localStorage.setItem('comp-password', companyPasswordInput.value);
+  // Convert image to Base64 string
+  const fileReader = new FileReader();
+  fileReader.onload = function(event) {
+    const companyLogoBase64 = event.target.result;
 
-  window.location.href = 'http://127.0.0.1:5500/company/.app/company-app.html';
+    // Store data in localStorage
+    localStorage.setItem('comp-name', companyNameInput.value);
+    localStorage.setItem('comp-email', companyEmailInput.value);
+    localStorage.setItem('comp-password', companyPasswordInput.value);
+    localStorage.setItem('comp-logo', companyLogoBase64);
+
+    // Redirect to company app
+    window.location.href = 'http://127.0.0.1:5500/company/.app/company-app.html';
+  }
+  fileReader.readAsDataURL(companyLogoInput.files[0]);
 });
