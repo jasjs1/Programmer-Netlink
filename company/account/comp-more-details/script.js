3e6f320226eecafab2ccdf684e9f2517911f6b14
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-05-01 20:19:21 -0500

    updated redirect url

--- company/account/comp-more-details/script.js
+++ company/account/comp-more-details/script.js
@@ -1,8 +1,10 @@
+const form = document.querySelector('form');
 const companyUsername = document.getElementById('comp-username');
 const companyTechStack = document.getElementById('comp-tech-stack');
+const companyURL = document.getElementById('comp-url');
+const hiring = document.getElementById('hiring');
 
 form.addEventListener('submit', function(event) {
-
   event.preventDefault();
 
   // Convert image to Base64 string
@@ -10,15 +12,15 @@ form.addEventListener('submit', function(event) {
   fileReader.onload = function(event) {
     const companyLogoBase64 = event.target.result;
 
-    localStorage.setItem('comp-username', companyUsername.value)
-    localStorage.setItem('comp-tech-stack', companyTechStack.value)
-
-    // Redirect to company app
-    window.location.href = 'http://127.0.0.1:5500/company/.app/company-app.html';
+    localStorage.setItem('comp-username', companyUsername.value);
+    localStorage.setItem('comp-tech-stack', companyTechStack.value);
+    localStorage.setItem('comp-url', companyURL.value);
+    localStorage.setItem('hiring', hiring.value);
   }
+
+  fileReader.readAsDataURL(companyLogo.files[0]);
 });
 
-
 function redirectToCompanyApp() {
-      window.location.href = 'http://127.0.0.1:5500/company/.app/company-app.html';
+  window.location.href = 'http://127.0.0.1:5500/company/account/comp-more-details/phase3/more-details-ph3.html';
 }
