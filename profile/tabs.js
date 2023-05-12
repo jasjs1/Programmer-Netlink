afd4b558c3027aa8d802b1252266b7c747f55721
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-05-12 16:11:02 -0500

    first

---
+++ profile/tabs.js
@@ -0,0 +1,37 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Create the buttons dynamically
+  var tabButtonsContainer = document.getElementById("tabButtons");
+  
+  var snippetsButton = document.createElement("button");
+  snippetsButton.className = "tablinks";
+  snippetsButton.innerHTML = "Snippets";
+  snippetsButton.onclick = function(event) {
+    openCity(event, "Snippets");
+  };
+  
+  var articlesButton = document.createElement("button");
+  articlesButton.className = "tablinks";
+  articlesButton.innerHTML = "Articles";
+  articlesButton.onclick = function(event) {
+    openCity(event, "Articles");
+  };
+  
+  tabButtonsContainer.appendChild(snippetsButton);
+  tabButtonsContainer.appendChild(articlesButton);
+  
+  // Show the "Settings" tab by default
+  document.getElementById("defaultOpen").click();
+  
