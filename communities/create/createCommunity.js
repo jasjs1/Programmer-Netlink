91344d9fec4a39333a8f83d46fcbc5c0a64c570a
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-06-06 20:45:45 -0500

    added location.href

--- communities/create/createCommunity.js
+++ communities/create/createCommunity.js
@@ -22,4 +22,5 @@ document.getElementById("create-community-form").addEventListener("submit", func
 
     // Clear form fields
     document.getElementById("create-community-form").reset();
+    location.href = 'http://127.0.0.1:5500/communities/displayCreatedCommunities/created-communities.html';
 });
