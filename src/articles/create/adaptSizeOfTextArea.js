a43950eff90af9494ca6fea076970412372f4aa1
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2024-01-15 14:33:00 -0600

    rm src

--- src/articles/create/adaptSizeOfTextArea.js
+++
@@ -1,6 +0,0 @@
-const textarea = document.getElementById('article-content'); // Replace example-element with the ID of your HTML textarea element.
-
-textarea.addEventListener('input', function() {
-  this.style.height = 'auto';
-  this.style.height = this.scrollHeight + 'px';
-});
